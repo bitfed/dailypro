5c868f6a144fb217a425316cf3bfee26cf2211ca
bitfed <bitfed@live.com>
2014-05-10 21:09:06 -0700

    easy009 created

---
+++ easy009.js
@@ -0,0 +1,20 @@
+/*jslint node: true */
+//"use strict";
+
+
+
+
+/*** CHALLENGE #9 ***
+
+  Write a program that will allow the user to input digits, and arrange them in
+  numerical order.
+
+  For extra credit, have it also arrange strings in alphabetical order.
+
+
+* http://www.reddit.com/r/dailyprogrammer/comments/pu1rf/2172012_challenge_9_easy/
+*/
+
+/*** NOTES ***
+
+*/
