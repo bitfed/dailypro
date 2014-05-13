ca6d5db70914d43384b9e36377e4afe37cf16ffd
bitfed <bitfed@live.com>
2014-05-13 03:33:45 -0700

    easy011 created

---
+++ easy011.js
@@ -0,0 +1,19 @@
+/*jslint node: true */
+//"use strict";
+
+
+
+
+/*** CHALLENGE #11 ***
+
+  The program should take three arguments. The first will be a day, the second
+  will be month, and the third will be year. Then, your program should compute
+  the day of the week that date will fall on.
+
+
+* http://www.reddit.com/r/dailyprogrammer/comments/pwons/2192012_challenge_11_easy/
+*/
+
+/*** NOTES ***
+
+*/
